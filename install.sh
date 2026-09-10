253b1ba8cfbdab73d17809075c0bc010567e1360
MARCELOSALVATIERRA926 <MARCELOSALVATIERRA926@GMAIL.COM>
2026-09-10 19:01:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+# ==============================================
+#   FENIX-SCR — ENTRADA PRINCIPAL
+#       Fenix 3815569765
+# ==============================================
+
+clear
+echo -e "
+════════════════════════════════════════════════════════
+        FENIX-SCR — INICIANDO
+        Fenix 3815569765
+════════════════════════════════════════════════════════
+"
+
+wget -q -O /tmp/fenix-instalador "https://raw.githubusercontent.com/MARCELOSALVATIERRA926/PHENIXSCR/main/fenix-instalador"
+chmod +x /tmp/fenix-instalador
+exec /tmp/fenix-instalador
 
